92ecd3554d0ec3a0726f9c56c585ea29231377d1
samuelcpr <samuelcpr464@gmail.com>
2022-08-22 17:33:42 -0300

    chore: integration CreateUserController

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -2,8 +2,11 @@ import { Request, Response, response } from "express"
 
 class CreateUserController {
     handle(request: Request, response: Response) {
+        const name = request.body.name;
 
+        return response.status(201).json({ message: `Usuário ${name} criado` })
     }
+
 }
 
 export { CreateUserController }
