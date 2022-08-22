a54f668fef044336a68835984263d16f0c9120b0
samuelcpr <samuelcpr464@gmail.com>
2022-08-22 16:01:16 -0300

    chore: created routes

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from "express"
+import { CreateUserController } from "./controllers/CreateUserController";
+
+const router = Router();
+
+const createUserController = new CreateUserController
+
+router.get("/", (request: Request, response: Response) => {
+    return response.json({ message: "Bem vindo a minha API" })
+})
+
+
+router.post("/users", createUserController.handle)
+
+export { router }
