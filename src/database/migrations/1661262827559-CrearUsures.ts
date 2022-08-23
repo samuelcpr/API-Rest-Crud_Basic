b1404c76fd4c78031a36753042b190b5e789a8ff
samuelcpr <samuelcpr464@gmail.com>
2022-08-23 11:02:07 -0300

    feat: created migrations

---
+++ src/database/migrations/1661262827559-CrearUsures.ts
@@ -0,0 +1,35 @@
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CrearUsures1661262827559 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "string",
+                        isPrimary: true,
+                        isNullable: false
+                    },
+                    {
+                        name: "name",
+                        type: "string",
+                        isNullable: false
+                    },
+                    {
+                        name: "email",
+                        type: "string",
+                        isNullable: true
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users")
+    }
+
+}
