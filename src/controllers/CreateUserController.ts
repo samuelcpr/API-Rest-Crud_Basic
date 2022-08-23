6390c125cd60571aee3faab68f3fb174b025144b
samuelcpr <samuelcpr464@gmail.com>
2022-08-23 09:50:10 -0300

    chore: created authentication

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,8 +1,15 @@
-import { Request, Response, response } from "express"
+import { Request, Response } from "express"
+import { CreateUserSevice } from "../services/CreateUserService"
 
 class CreateUserController {
     handle(request: Request, response: Response) {
+
+        const createUserSevice = new CreateUserSevice
         const name = request.body.name;
+
+        if (name.length === 0) {
+            return response.status(400).json({ mensagem: "informe um nome de usuário" })
+        }
 
         return response.status(201).json({ message: `Usuário ${name} criado` })
     }
